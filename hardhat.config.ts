3dd450f19966f8d92fa0fff1b8d1f274e7bcd443
d0x4545lit <dev@deelit.net>
2024-05-30 15:41:10 +0200

    Add etherscan plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,18 @@
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import '@openzeppelin/hardhat-upgrades';
-
+import "@nomicfoundation/hardhat-verify";
 import 'solidity-docgen';
 // import "hardhat-gas-reporter"
 
 const SEPOLIA_ALCHEMY_API_KEY = vars.get("SEPOLIA_ALCHEMY_API_KEY");
+const SEPOLIA_ETHSCAN_API_KEY = vars.get("SEPOLIA_ETHSCAN_API_KEY");
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 const BASE_SEPOLIA_ALCHEMY_API_KEY = vars.get("BASE_SEPOLIA_ALCHEMY_API_KEY");
+const BASE_SEPOLIA_ETHSCAN_API_KEY = vars.get("BASE_SEPOLIA_ETHSCAN_API_KEY");
 const BASE_SEPOLIA_PRIVATE_KEY = vars.get("BASE_SEPOLIA_PRIVATE_KEY");
 
-// const ARBITRUIM_ALCHEMY_API_KEY = vars.get("ARBITRUIM_ALCHEMY_API_KEY");
-// const ARBITRUIM_SEPOLIA_PRIVATE_KEY = vars.get("ARBITRUIM_SEPOLIA_PRIVATE_KEY");
-
-// const OP_SEPOLIA_ALCHEMY_API_KEY = vars.get("OP_SEPOLIA_ALCHEMY_API_KEY");
-// const OP_SEPOLIA_PRIVATE_KEY = vars.get("OP_SEPOLIA_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -30,6 +27,12 @@ const config: HardhatUserConfig = {
   docgen: {
     outputDir: './docs',
   },
+  etherscan: {
+    apiKey: {
+      sepolia: SEPOLIA_ETHSCAN_API_KEY,
+      baseSepolia: BASE_SEPOLIA_ETHSCAN_API_KEY
+    },
+  },
   networks: { 
     hardhat: {
       chainId: 1337
@@ -38,7 +41,7 @@ const config: HardhatUserConfig = {
       url: `https://eth-sepolia.g.alchemy.com/v2/${SEPOLIA_ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
     },
-    base_sepolia: {
+    baseSepolia: {
       url: `https://base-sepolia.g.alchemy.com/v2/${BASE_SEPOLIA_ALCHEMY_API_KEY}`,
       accounts: [BASE_SEPOLIA_PRIVATE_KEY]
     }

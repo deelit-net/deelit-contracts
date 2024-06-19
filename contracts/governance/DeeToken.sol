e061efc7ff8c2713756172f682e076b895726785
d0x4545lit <dev@deelit.net>
2024-06-19 14:40:52 +0200

    Add token governance contract

---
+++ contracts/governance/DeeToken.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+
+/// @title Governance token for Deelit Protocol
+/// @author d0x4545lit
+/// @custom:security-contact dev@deelit.net
+contract DeeToken is ERC20, ERC20Permit, ERC20Votes {
+
+    constructor() 
+        ERC20 ("DeeToken", "DEE") 
+        ERC20Permit("DeeToken") {
+            
+        // mint 1M tokens to the deployer
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+    }
+
+    // Overrides IERC6372 functions to make the token & governor timestamp-based
+    function clock() public view override returns (uint48) {
+        return uint48(block.timestamp);
+    }
+
+    // solhint-disable-next-line func-name-mixedcase
+    function CLOCK_MODE() public pure override returns (string memory) {
+        return "mode=timestamp";
+    }
+
+    // The functions below are overrides required by Solidity.
+    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, amount);
+    }
+
+    function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+
+}
